da0e67bd599a559b0b49cbfef744ebd3fbd3eeec
LouiseWJ <wanju412@gmail.com>
2022-12-30 19:34:20 +0800

    feat fs

--- Practice/server_html/main.js
+++ Practice/server_html/main.js
@@ -2,24 +2,31 @@ const port = 3000
 const http = require('http')
 const httpStatus = require('http-status-codes')
 const fs = require('fs')
-const routeMap = {
-    '/': 'views/index.html'
-}
+const getViewUrl = (url) => {
+    return `views${url}.html`;
+};
+
 
 http
     .createServer((req, res) => {
-        res.writeHead(httpStatus.OK, {
-            'Content-Type': 'text/html'
-        });
-        if (routeMap[req.url]) {
-            // when read files, files could be corrupt, unreadable or missing
-            fs.readFile(routeMap[req.url], (error, data) => {
+        let viewUrl = getViewUrl(req.url);
+        fs.readFile(viewUrl, (error, data) => {
+            if (error) {
+                res.writeHead(httpStatus.NOT_FOUND);
+                res.write('<h1>FILE NOT FOUND</h1>');
+            } else {
+                res.writeHead(httpStatus.OK, {
+                    'Content-Type': 'text/html'
+                });
                 res.write(data);
-                res.end();
-            });
-        } else {
-            res.end('<h1>Sorry, not found.</h1>');
-        }
+            }
+            res.end();
+        });
     })
     .listen(port);
+
 console.log(`The server has started and is listening on port number: ${port}`);
+
+
+
+
