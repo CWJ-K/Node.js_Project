37bbe9e2e3a260c0ddd03c5876c4b6ba312031af
CWJ-K <wanju412@gmail.com>
2023-01-28 11:29:58 +0800

    feat: main, folder structure

---
+++ Practice/L12_confettit_cuisine/main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Confetti Cuisine!');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(
+        `Server running at http://localhost:${app.get('port')}`
+    );
+});
