9acb25e87818805e946bb4097f22f156fd2d6dd8
CWJ-K <wanju412@gmail.com>
2023-01-30 20:25:43 +0800

    feat: L12 express project

--- Practice/L12_confettit_cuisine/main.js
+++ Practice/L12_confettit_cuisine/main.js
@@ -1,18 +1,33 @@
+'use strict';
+
 const express = require('express');
 const app = express();
+const homeController = require('./controllers/homeController');
+const layouts = require('express-ejs-layouts');
+const errorController = require('./controllers/errorController')
 
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
 
+app.use(layouts);
 app.use(
     express.urlencoded({
         extended: false
     })
 );
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Confetti Cuisine!');
+    res.render('index');
 });
+
+app.get('/courses', homeController.showCourses);
+app.get('/contact', homeController.showSignUp);
+app.post('/contact', homeController.postedSignUpForm);
+
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
 
 app.listen(app.get('port'), () => {
     console.log(
